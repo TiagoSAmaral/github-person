5172b6164510a389a647b4cd13cba7bea10d238e
Tiago Amaral <tiagofly@outlook.com>
2023-06-13 23:45:50 -0300

    Update NetworkUserInfo.swift Melhoria de codigo para criar requisicoes.

--- source/app/Shared/Network/NetworkUserInfo.swift
+++ source/app/Shared/Network/NetworkUserInfo.swift
@@ -9,113 +9,201 @@
 import Alamofire
 import Foundation
 
-typealias ResultUsersItemOrError = Result<[UserDetailProfile], NetworkError>
+typealias ResultUser = Result<[UserDetailProfile]?, NetworkError>
+typealias ResultRepos = Result<[RepoListemItem]?, NetworkError>
+typealias ResultUserContentHandler = ((ResultUser) -> Void)
+typealias ResultContainerContentHandler = ((ResultRepos) -> Void)
 
 protocol NetworkUserInfoOperation {
-    func search(with name: String?, handler: ((Result<[UserDetailProfile], NetworkError>) -> Void)?)
-    func requestListUser(page: Int?, params: [String: Any]?, handler: ((Result<[UserDetailProfile], NetworkError>) -> Void)?)
-    func requestUser(with name: String?, handler: ((ResultUsersItemOrError) -> Void)?)
-    func requestUserRepos(name: String?, page: Int?, params: [String: Any]?, handler: ((Result<[RepoListemItem], NetworkError>) -> Void)?)
+    func makSearchRequest(with params: RequestParams?, handler: ResultUserContentHandler?)
+    func makeListUserRequest(with params: RequestParams?, handler: ResultUserContentHandler?)
+    func makeUserDetailRequest(with params: RequestParams?, handler: ResultUserContentHandler?)
+    func makeListRepositoriesRequest(with params: RequestParams?, handler: ResultContainerContentHandler?)
 }
 
 class NetworkUserInfo: NetworkUserInfoOperation, NetworkConectable {
     
     var headerFactory: NetworkHeaderFactory?
+    var routesMarker: ApiRoutes?
     
     init(headerFactory: NetworkHeaderFactory) {
         self.headerFactory = headerFactory
     }
     
-    func search(with name: String?, handler: ((Result<[UserDetailProfile], NetworkError>) -> Void)?) {
+    func makSearchRequest(with params: RequestParams?, handler: ResultUserContentHandler?) {
+        guard let params = params else {
+            handler?(.failure(NetworkError.badRequest(text: "Params not defined")))
+            return
+        }
+       let paramsApi = buildingRequest(with: routesMarker?.makeUrlSearch(with: params))
         
-        let request: RequestApi = RequestApi(
-            urlPath: ApiRoutes.shared.path(.getSearchUser(text: name)),
-                              method: Alamofire.HTTPMethod.get,
-                              params: nil,
-                              headers: headerFactory?.makeHeader())
-        networkRequest(data: request, resultType: SearchResult.self) { response in
+        networkRequest(params: paramsApi, resultType: SearchResult.self) { response in
             switch response {
-            case .success(let result):
-                
-                guard let item = result.items else {
-                    handler?(.failure(NetworkError.makeError(with: nil, description: nil)))
-                    return
-                }
-                
-                let itemsWithLayout: [UserDetailProfile] = item.compactMap { (item: UserDetailProfile) in
-                    item.layout = .userListItem
-                    return item
-                }
-                 
-                handler?(.success(itemsWithLayout))
+            case .success(let searchResult):
+                let items = searchResult.items?.compactMap({ $0.layout = params.layout; return $0})
+                handler?(.success(items))
             case .failure(let error):
                 handler?(.failure(error))
             }
         }
     }
     
-    func requestListUser(page: Int?, params: [String: Any]?, handler: ((Result<[UserDetailProfile], NetworkError>) -> Void)?) {
-        let request: RequestApi = RequestApi(
-                              urlPath: ApiRoutes.shared.path(.getListUser(page: page)),
-                              method: Alamofire.HTTPMethod.get,
-                              params: params,
-                              headers: headerFactory?.makeHeader())
-        networkRequest(data: request, resultType: [UserDetailProfile].self) { result in
+    func makeListUserRequest(with params: RequestParams?, handler: ResultUserContentHandler?) {
+        guard let params = params else {
+            handler?(.failure(NetworkError.badRequest(text: "Params not defined")))
+            return
+        }
 
-            var itemsWithLayout: [UserDetailProfile] = []
-            switch result {
-            case .success(let items):
-                
-                itemsWithLayout = items.compactMap { item in
-                    item.layout = .userListItem
-                    return item
-                }
-                
-                handler?(.success(itemsWithLayout))
-                
-            case .failure(let error):
-                handler?(.failure(error))
-            }
-        }
-    }
-    
-    func requestUser(with name: String?, handler: ((ResultUsersItemOrError) -> Void)?) {
-        
-        let request: RequestApi = RequestApi(
-            urlPath: ApiRoutes.shared.path(.getUserProfile(text: name)),
-                              method: Alamofire.HTTPMethod.get,
-                              params: nil,
-                              headers: headerFactory?.makeHeader())
-        networkRequest(data: request, resultType: UserDetailProfile.self) { response in
+        let paramsApi = buildingRequest(with: routesMarker?.makeUrlListUsers(with: params))
+        networkRequest(params: paramsApi, resultType: [UserDetailProfile].self) { response in
             switch response {
-            case .success(let user):
-                user.layout = .userInfo
-                handler?(.success([user]))
+            case .success(let items):
+                let itemsWithLayout = items.compactMap({ $0.layout = params.layout; return $0 })
+                handler?(.success(itemsWithLayout))
             case .failure(let error):
                 handler?(.failure(error))
             }
         }
     }
     
-    func requestUserRepos(name: String?, page: Int?, params: [String: Any]?, handler: ((Result<[RepoListemItem], NetworkError>) -> Void)?) {
-        let request: RequestApi = RequestApi(
-            urlPath: ApiRoutes.shared.path(.getListRepoFromUser(text: name, page: page)),
-                              method: Alamofire.HTTPMethod.get,
-                              params: params,
-                              headers: headerFactory?.makeHeader())
-        networkRequest(data: request, resultType: [RepoListemItem].self) { result in
-            
-            switch result {
-            case .success(let items):
-                let itemsLayoutMaped: [RepoListemItem] = items.compactMap { item in
-                    var mutableItem = item
-                    mutableItem.layout = .repoListItem
-                    return mutableItem
-                }
-                handler?(.success(itemsLayoutMaped))
+    func makeUserDetailRequest(with params: RequestParams?, handler: ResultUserContentHandler?) {
+        guard let params = params else {
+            handler?(.failure(NetworkError.badRequest(text: "Params not defined")))
+            return
+        }
+
+        let paramsApi = buildingRequest(with: routesMarker?.makeUrlUserDetail(with: params))
+        networkRequest(params: paramsApi, resultType: UserDetailProfile.self) { response in
+            switch response {
+            case .success(let item):
+                item.layout = params.layout
+                handler?(.success([item]))
             case .failure(let error):
-                handler?(.failure(error) )
+                handler?(.failure(error))
             }
         }
     }
+    
+    func makeListRepositoriesRequest(with params: RequestParams?, handler: ResultContainerContentHandler?) {
+        guard let params = params else {
+            
+            handler?(.failure(NetworkError.badRequest(text: "Params not defined")))
+            return
+        }
+        
+        let paramsApi = buildingRequest(with: routesMarker?.makeUrlRepositories(with: params))
+        networkRequest(params: paramsApi, resultType: [RepoListemItem].self) { response in
+            switch response {
+            case .success(let items):
+                let itemsWithLayout = items.compactMap({ var mutable = $0; mutable.layout = params.layout; return mutable})
+                handler?(.success(itemsWithLayout))
+            case .failure(let error):
+                handler?(.failure(error))
+            }
+        }
+    }
+    
+    private func buildingRequest(with urlPath: URL?) -> APIParams {
+        let requestApi = APIParams()
+        requestApi.urlPath = urlPath
+        requestApi.method = .get
+        requestApi.headers = headerFactory?.makeHeader()
+        return requestApi
+    }
+     
+//    func search(with params: RequestParams?, handler: ResultContent) {
+//
+//        let urlPath = routesMarker?.makeUrlSearch(with: params)
+//
+//        let request: RequestApi = RequestApi(
+//            urlPath: ApiRoutes.shared.path(.getSearchUser(text: name)),
+//                              method: Alamofire.HTTPMethod.get,
+//                              params: nil,
+//                              headers: headerFactory?.makeHeader())
+//
+//        networkRequest(data: request, resultType: SearchResult.self) { response in
+//            switch response {
+//            case .success(let result):
+//
+//                guard let item = result.items else {
+//                    handler?(.failure(NetworkError.makeError(with: nil, description: nil)))
+//                    return
+//                }
+//
+//                let itemsWithLayout: [UserDetailProfile] = item.compactMap { (item: UserDetailProfile) in
+//                    item.layout = .userListItem
+//                    return item
+//                }
+//
+//                handler?(.success(itemsWithLayout))
+//            case .failure(let error):
+//                handler?(.failure(error))
+//            }
+//        }
+//    }
+    
+//    func requestListUser(page: Int?, params: [String: Any]?, handler: ((Result<[UserDetailProfile], NetworkError>) -> Void)?) {
+//        let request: RequestApi = RequestApi(
+//                              urlPath: ApiRoutes.shared.path(.getListUser(page: page)),
+//                              method: Alamofire.HTTPMethod.get,
+//                              params: params,
+//                              headers: headerFactory?.makeHeader())
+//        networkRequest(data: request, resultType: [UserDetailProfile].self) { result in
+//
+//            var itemsWithLayout: [UserDetailProfile] = []
+//            switch result {
+//            case .success(let items):
+//
+//                itemsWithLayout = items.compactMap { item in
+//                    item.layout = .userListItem
+//                    return item
+//                }
+//
+//                handler?(.success(itemsWithLayout))
+//
+//            case .failure(let error):
+//                handler?(.failure(error))
+//            }
+//        }
+//    }
+    
+//    func requestUser(with name: String?, handler: ((ResultUsersItemOrError) -> Void)?) {
+//
+//        let request: RequestApi = RequestApi(
+//            urlPath: ApiRoutes.shared.path(.getUserProfile(text: name)),
+//                              method: Alamofire.HTTPMethod.get,
+//                              params: nil,
+//                              headers: headerFactory?.makeHeader())
+//        networkRequest(data: request, resultType: UserDetailProfile.self) { response in
+//            switch response {
+//            case .success(let user):
+//                user.layout = .userInfo
+//                handler?(.success([user]))
+//            case .failure(let error):
+//                handler?(.failure(error))
+//            }
+//        }
+//    }
+    
+//    func requestUserRepos(name: String?, page: Int?, params: [String: Any]?, handler: ((Result<[RepoListemItem], NetworkError>) -> Void)?) {
+//        let request: RequestApi = RequestApi(
+//            urlPath: ApiRoutes.shared.path(.getListRepoFromUser(text: name, page: page)),
+//                              method: Alamofire.HTTPMethod.get,
+//                              params: params,
+//                              headers: headerFactory?.makeHeader())
+//        networkRequest(data: request, resultType: [RepoListemItem].self) { result in
+//
+//            switch result {
+//            case .success(let items):
+//                let itemsLayoutMaped: [RepoListemItem] = items.compactMap { item in
+//                    var mutableItem = item
+//                    mutableItem.layout = .repoListItem
+//                    return mutableItem
+//                }
+//                handler?(.success(itemsLayoutMaped))
+//            case .failure(let error):
+//                handler?(.failure(error) )
+//            }
+//        }
+//    }
 }

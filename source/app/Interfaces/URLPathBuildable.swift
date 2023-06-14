1d51ea861b9ad5a415922cb8bbc1057f0d48bca2
Tiago Amaral <tiagofly@outlook.com>
2023-06-13 23:51:36 -0300

    Update URLPathBuildable.swift Interface  para criar URL`s e não deixar classe cliente vinculada a implementacao concreta da classe que constroi a URL.

---
+++ source/app/Interfaces/URLPathBuildable.swift
@@ -0,0 +1,16 @@
+//
+//  URLPathBuildable.swift
+//  github-person
+//
+//  Created by Tiago Amaral on 13/06/23.
+//  Copyright © 2023 developer_organization_name. All rights reserved.
+//
+
+import Foundation
+
+protocol URLPathBuildable {
+    func makeUrlListUsers(with params: RequestParams?) -> URL?
+    func makeUrlSearch(with params: RequestParams?) -> URL?
+    func makeUrlUserDetail(with params: RequestParams?) -> URL?
+    func makeUrlRepositories(with params: RequestParams?) -> URL?
+}
